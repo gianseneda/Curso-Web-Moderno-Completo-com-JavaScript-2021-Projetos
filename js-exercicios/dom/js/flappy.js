bd034bccd6fd944e45f85b865bce7bbe975861d3
gian seneda <gian_seneda@hotmail.com>
2021-04-30 13:52:22 -0300

    package json

--- js-exercicios/dom/js/flappy.js
+++ js-exercicios/dom/js/flappy.js
@@ -17,6 +17,34 @@ function Barreira(reversa = false) {
   this.setAltura = (altura) => (corpo.style.height = `${altura}px`);
 }
 
-const b = new Barreira(false);
-b.setAltura(200);
-document.querySelector("[wm-flappy]").appendChild(b.elemento);
+// const b = new Barreira(false);
+// b.setAltura(200);
+// document.querySelector("[wm-flappy]").appendChild(b.elemento);
+
+function ParDeBarreiras(altura, abertura, x) {
+  this.elemento = novoElemento("div", "par-de-barreiras");
+
+  this.superior = new Barreira(true);
+  this.inferior = new Barreira(false);
+
+  this.elemento.appendChild(this.superior.elemento);
+  this.elemento.appendChild(this.inferior.elemento);
+
+  this.sortearAbertura = () => {
+    //deixando essa funçao publica, acessível fora da função
+    const alturaSuperior = Math.random() * (altura - abertura);
+    const alturaInferior = altura - abertura - alturaSuperior;
+    this.superior.setAltura(alturaSuperior);
+    this.inferior.setAltura(alturaInferior);
+  };
+
+  this.getX = () => parseInt(this.elemento.style.left.split("px")[0]); //retorna a altura sem o px, retorna um array, por isso o [0]
+  this.setX = (x) => (this.elemento.style.left = `${x}px`);
+  this.getLargura = () => this.elemento.clientWidth;
+
+  this.sortearAbertura();
+  this.setX(x);
+}
+
+// const b = new ParDeBarreiras(700, 300, 400);
+// document.querySelector("[wm-flappy]").appendChild(b.elemento);
