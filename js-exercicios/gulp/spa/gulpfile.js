af71df58a467cfa8fec80a6e9a5acbf11ca68ee6
gian seneda <gian_seneda@hotmail.com>
2021-05-05 14:54:00 -0300

    finalização de um porjeto para por em nível de predução SPA

---
+++ js-exercicios/gulp/spa/gulpfile.js
@@ -0,0 +1,12 @@
+const { series, parallel } = require("gulp");
+const gulp = require("gulp");
+
+const { appHTML, appCSS, appJS, appIMG } = require("./gulpTasks/app");
+const { depsCSS, depsFONTS } = require("./gulpTasks/deps");
+const { monitorarArquivos, servidor } = require("./gulpTasks/servidor");
+
+module.exports.default = series(
+  parallel(series(appHTML, appCSS, appJS, appIMG), series(depsCSS, depsFONTS)),
+  servidor,
+  monitorarArquivos
+);
